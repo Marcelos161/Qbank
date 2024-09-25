38c69736457000fac0772620ea78b8e096182f0d
Marcelo Diogo <marcelos.diogos@outlook.com>
2024-09-25 18:33:50 -0300

    adiçao de arquitetura MVC

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using QBankApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -35,45 +37,4 @@ app.MapDelete("/accounts/{id}", (int id) =>
     // Código para deletar uma conta
 });
 
-app.MapGet("/pessoa", () =>
-{
-    var pessoa = new Pessoa
-    {
-        Nome = "Fulano de Tal",
-        DataNascimento = new DateTime(1980, 1, 1),
-        Email = "fulano@fulano.com",
-        Telefone = "11 99999-9999",
-        Endereco = "Rua das Flores",
-        Cidade = "São Paulo",
-        Estado = "SP",
-        Pais = "Brasil",
-        CEP = "00000-000",
-        CPF = "000.000.000-00",
-        RG = "00.000.000-0",
-        CNH = "00000000000",
-    };
-    return pessoa;
-});
-
-namespace ApiAula1
-{
-    public class Pessoa
-    {
-        public string Nome { get; set; } = string.Empty;
-        public DateTime DataNascimento { get; set; }
-        public string Email { get; set; } = string.Empty;
-        public string Telefone { get; set; } = string.Empty;
-        public string Endereco { get; set; } = string.Empty;
-        public string Cidade { get; set; } = string.Empty;
-        public string Estado { get; set; } = string.Empty;
-        public string Pais { get; set; } = string.Empty;
-        public string CEP { get; set; } = string.Empty;
-        public string CPF { get; set; } = string.Empty;
-        public string RG { get; set; } = string.Empty;
-        public string CNH { get; set; } = string.Empty;
-        public string TituloEleitor { get; set; } = string.Empty;
-        public string Passaporte { get; set; } = string.Empty;
-        public string CarteiraTrabalho { get; set; } = string.Empty;
-    }
-}
 app.Run();
